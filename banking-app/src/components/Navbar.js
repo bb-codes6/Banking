057317ab8686f3901171e6bf4c48cf611c9bb477
Breanna B. <88126555+bb-codes6@users.noreply.github.com>
2025-01-05 22:30:47 +0000

    created Navbar component

---
+++ banking-app/src/components/Navbar.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Navbar() {
+    return (
+        <nav style={{padding: "10px", backgroundColor: "#282c34", color: "#fff"}}>
+        <h1>Banking App</h1>
+    </nav>
+    );
+}
+
+export default Navbar;
