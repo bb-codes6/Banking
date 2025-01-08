5a4990405e708f1d6bc806aaacffdc86dc91da1c
Breanna Blanks <breanna_brownlee@yahoo.com>
2025-01-08 09:38:35 -0600

    added other pages

--- banking-app/src/App.js
+++ banking-app/src/App.js
@@ -1,23 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+// src/App.js
+import React, { useState } from "react";
+import Navbar from "./components/Navbar";
+import Login from "./components/Login";
+import Dashboard from "./components/Dashboard";
+import TransactionHistory from "./components/TransactionHistory";
 
 function App() {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Navbar />
+      {!isLoggedIn ? (
+        <Login onLogin={setIsLoggedIn} />
+      ) : (
+        <>
+          <Dashboard />
+          <TransactionHistory />
+        </>
+      )}
     </div>
   );
 }
